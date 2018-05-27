b6dd058fde3092263a7d0ce76ec3ed1f67714170
Juan Diego <jdcm10@gmail.com>
2018-05-26 21:07:08 -0500

    haciendo animacion

--- js/trans.js
+++ js/trans.js
@@ -1,4 +1,32 @@
 document.addEventListener("DOMContentLoaded", function() {
-	Barba.Pjax.init();
-	Barba.Prefetch.init();
+ 	
+ 	Barba.Pjax.start();
+  	Barba.Prefetch.init();
+    
+    var FadeTransition = Barba.BaseTransition.extend({
+      start: function() {
+        Promise
+          .all([this.newContainerLoading, this.fadeOut()])
+          .then(this.fadeIn.bind(this));
+      },
+ 
+      fadeOut: function() {
+        this.oldContainer.classList.toggle('fade-out');
+        return new Promise(function(resolve, reject) {
+            setTimeout(function() {
+               resolve();
+           }, 500);
+        });
+      },
+ 
+      fadeIn: function() {
+        this.newContainer.classList.toggle('fade-in');
+        this.done();
+      }
+    });
+
+    Barba.Pjax.getTransition = function() {
+      return FadeTransition;
+    };
+	
 });
